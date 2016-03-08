685bc750167f32de702b0e9ce8ff7b2b829cf5f2
Dirk Marciniak <dirk_marciniak@online.de>
2016-03-08 23:47:54 +0100

    gemeinsame Variable und Funktion zugefügt

--- ANDROID/btLeHomeLightApp/src/main/java/de/dmarcini/bt/btlehomelight/fragments/lightRootFragment.java
+++ ANDROID/btLeHomeLightApp/src/main/java/de/dmarcini/bt/btlehomelight/fragments/lightRootFragment.java
@@ -2,6 +2,7 @@ package de.dmarcini.bt.btlehomelight.fragments;
 
 import android.app.Fragment;
 
+import de.dmarcini.bt.btlehomelight.ProjectConst;
 import de.dmarcini.bt.btlehomelight.interfaces.IBtServiceListener;
 
 /**
@@ -9,4 +10,31 @@ import de.dmarcini.bt.btlehomelight.interfaces.IBtServiceListener;
  */
 public abstract class LightRootFragment extends Fragment implements IBtServiceListener
 {
+  protected long timeToSend;
+
+  /**
+   * Lese die Farbparameter aus dem Kommando und speichere diese in meinem Array
+   *
+   * @param param Array mit Kommandowerten
+   */
+  protected short[] fillValuesInArray(String[] param)
+  {
+    short[] rgbw = new short[ 4 ];
+    //
+    // der erste Parameter ist das Kommando, den ignoriere ich mal
+    //
+    for( int i = 1; i < ProjectConst.C_ASKRGB_LEN; i++ )
+    {
+      try
+      {
+        rgbw[ i - 1 ] = Short.parseShort(param[ i ], 16);
+      }
+      catch( NumberFormatException ex )
+      {
+        rgbw[ i - 1 ] = 0;
+      }
+    }
+    return (rgbw);
+  }
+
 }
